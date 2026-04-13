387da58b1d4198e3559ed538d9c40c9c068f64a6
Saurav-Bhattacharjee <Saroh2009@gmail.com>
2026-04-13 10:12:58 -0400

    Task 3 done

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     #Function to analyse the text 
@@ -6,5 +7,10 @@ def emotion_detector(text_to_analyze):
     headers={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload={ "raw_document": { "text": text_to_analyze } } 
     response = requests.post(url, json=payload, headers=headers)
-    return response.text
+    data=json.loads(response.text)
+    all_emotion=data['emotionPredictions'][0]['emotion']
+    dominant = max(all_emotion,key=all_emotion.get)
+    all_emotion['dominant_emotion']= dominant
+    return all_emotion
+
      
